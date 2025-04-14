345484cdc9b05485a618c3945874de3c9e9d5e59
Janelle Choi <janellec@unc.edu>
2025-04-14 00:08:11 -0400

    progress on ex.04

--- exercises/ex04/river_simulation.py
+++ exercises/ex04/river_simulation.py
@@ -1,1 +1,15 @@
+"""File to model daily life in river ecosystem."""
 
+from exercises.ex04.river import River
+
+
+my_river = River(10, 2)
+
+
+def view_river(self):
+    print(f"~~~ Day {self.day}: ~~~")
+    print(f"Fish population: {len(self.fish)}")
+    print(f"Bear population: {len(self.bears)}")
+
+
+my_river.view_river()
